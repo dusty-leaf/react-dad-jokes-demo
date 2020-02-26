c3399171eba1fe10c0df476774e9de0f9150bafd
alec-sena <alexanderwsena@gmail.com>
2020-02-26 11:19:13 -0500

    Updates

---
+++ precache-manifest.66e38c980635aa0495836702b79d1416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0844731e08fa174322a897f550e206",
+    "url": "/react-dad-jokes-demo/index.html"
+  },
+  {
+    "revision": "298525d2a58d55a17f56",
+    "url": "/react-dad-jokes-demo/static/css/main.a208b4a9.chunk.css"
+  },
+  {
+    "revision": "2c70f58367b317820c9c",
+    "url": "/react-dad-jokes-demo/static/js/2.1b6d635a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-dad-jokes-demo/static/js/2.1b6d635a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298525d2a58d55a17f56",
+    "url": "/react-dad-jokes-demo/static/js/main.f7424ce2.chunk.js"
+  },
+  {
+    "revision": "5ed135a6739f2c1c9d37",
+    "url": "/react-dad-jokes-demo/static/js/runtime-main.2177d65e.js"
+  }
+]);
